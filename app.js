1d46d484ea9f751255bcc6eb477be719de9c4ae6
Riley Childs <childswebdev@gmail.com>
2024-11-11 11:05:29 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware for serving static files
+app.use(express.static('public'));
+
+// Placeholder route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
